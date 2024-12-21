ef531eaaa9583b64e2692d619cfe7205d0ded067
Dmytro Khymytsia <dimahimitsya@gmail.com>
2024-12-21 11:02:33 +0100

    Added basic equations

--- main.c
+++ main.c
@@ -2,5 +2,26 @@
 
 int main()
 {
+    float num1, num2, res;
+    char sign;
+
+    printf("Hi, it`s terminal Calculator\n");
+    printf("Enter your first number: ");
+    scanf("%f", &num1);
+    printf("Enter your second number: ");
+    scanf("%f", &num2);
+
+    scanf("%c", &sign);     //For unnecessary enter
+
+    printf("Choose the equation operation: + - * /\n");
+    scanf("%c", &sign);
+
+    if(sign == '+') res = num1 + num2;
+    else if(sign == '-') res = num1 - num2;
+    else if(sign == '*') res = num1 * num2;
+    else if(sign == '/') res = num1 / num2;
+
+    printf("Result: %.2f\n", res);
+
     return 0;
 }
