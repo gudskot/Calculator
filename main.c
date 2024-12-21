1bf9bb9453e8cad3208c00f3037aaad9da24c6ec
Dmytro Khymytsia <dimahimitsya@gmail.com>
2024-12-21 17:30:14 +0100

    Added new features
    - added cycle for calculation
    - added opportunities for leaving and entrance

--- main.c
+++ main.c
@@ -3,25 +3,43 @@
 int main()
 {
     float num1, num2, res;
-    char sign;
+    char sign, quit;
 
     printf("Hi, it`s terminal Calculator\n");
-    printf("Enter your first number: ");
-    scanf("%f", &num1);
-    printf("Enter your second number: ");
-    scanf("%f", &num2);
+    while(quit != 'Q')
+    {
+        printf("Enter your first number: ");
+        scanf("%f", &num1);
+        printf("Enter your second number: ");
+        scanf("%f", &num2);
 
-    scanf("%c", &sign);     //For unnecessary enter
+        scanf("%c", &sign);      //For unnecessary enter
 
-    printf("Choose the equation operation: + - * /\n");
-    scanf("%c", &sign);
+        printf("Choose the equation operation: + - * /\n");
+        scanf("%c", &sign);
 
-    if(sign == '+') res = num1 + num2;
-    else if(sign == '-') res = num1 - num2;
-    else if(sign == '*') res = num1 * num2;
-    else if(sign == '/') res = num1 / num2;
+        switch(sign)
+        {
+            case '+':
+                res = num1 + num2;
+                break;
+            case '-':
+                res = num1 - num2;
+                break;
+            case '*':
+                res = num1 * num2;
+                break;
+            case '/':
+                res = num1 / num2;
+                break;
+        }
+        printf("Result: %.2f\n", res);
+        printf("Continue -> Enter\nLeave -> Q\n");
 
-    printf("Result: %.2f\n", res);
+        scanf("%c", &quit);        //For unnecessary enter
+
+        scanf("%c", &quit);
+    }
 
     return 0;
 }
